d39db5abe337d57c54cc08f06aec331b6541ba62
Ciarán Hyland <ciaranh96@gmail.com>
2017-02-16 13:53:10 +0000

    Fixed more stupid shit

--- Solution.java
+++ Solution.java
@@ -1,4 +1,4 @@
-import java.util.*
+import java.util.*;
 class Warehouse{
   int id,r,c;
   String[] products;
@@ -38,14 +38,14 @@ class Drone{
 
   public String toString(){
     String s = "Id: " + id + ", Payload: " + payload + "u\n(r,c) = " + r + "," + c + "\nProducts: ";
-    for(int i:products) s += i + " ";
+    for(String i:products) s += i + " ";
     return s;
   }
 }
 
 class Order{
   int id,r,c; //r,c are the location of the customer
-  int[] products;
+  String[] products;
 
   public Order(int id,int r,int c,String[] products){
     this.id = id;
@@ -80,7 +80,7 @@ class Grid{
     this.cols = cols;
     this.payload = payload;
     this.turns = turns;
-    this.products = products;
+    this.productQty = productQty;
     this.productWeights = productWeights;
     this.drones = drones;
     this.orders = orders;
@@ -145,20 +145,20 @@ public class Solution {
   	int cols = sc.nextInt();
   	int nDrones = sc.nextInt();
 
-  	int turns = = sc.nextInt();
+  	int turns  = sc.nextInt();
   	int payload = sc.nextInt();
   	sc.nextLine();
   	int nProd = sc.nextInt();
   	sc.nextLine();
   	String[] productsWeights = sc.nextLine().split(" ");
-  	int nWarehouse = in.nextInt();
+  	int nWarehouse = sc.nextInt();
   	Warehouse[] warehouses = new Warehouse[nWarehouse];
   	for(int i = 0;i < nWarehouse;i++){
   		int x = sc.nextInt();
   		int y = sc.nextInt();
-  		sc.nextLine|();
+  		sc.nextLine();
   		String[] prod = sc.nextLine().split(" ");
-  		W[i] = new Warehouse(i,x,y,prod);
+  		warehouses[i] = new Warehouse(i,x,y,prod);
   	}
   	int nOrders = sc.nextInt();
   	Order[] orders = new Order[nOrders];
@@ -168,8 +168,8 @@ public class Solution {
 		int n = sc.nextInt();
 		String[] prod = new String[n];
 		for(int j = 0; j < n;j++){
-			int x = sc.nextInt();
-			prod[x] = "1";
+			int c = sc.nextInt();
+			prod[c] = "1";
 			}
 			orders[i] = new Order(i,x,y,prod);
 	}
